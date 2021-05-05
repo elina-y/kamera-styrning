4bbb36e4fc10656c33577865bae56297600c3669
elina <elinayrlid@Elinas-MacBook-Pro.local>
2021-05-05 15:41:00 +0200

    Testat att räkna ut vinkeln för pan2

--- camera.py
+++ camera.py
@@ -29,9 +29,7 @@ global auth
 # B = avstandet från kameran till fokuspunkten
 #VIKTIGT: skriv "import math" i main <3
 
-#todo:
-#1. fixa hämta ip automatiskt.
-#2.
+
 
 # MAC LILLA 00408CB977FF
 # Mac STORA ACCC8ED91461
@@ -71,6 +69,7 @@ def calibrate() :
 
 
 def getB(S, tilt1):
+    #B är längden till punkten vi vill kolla på, S höjden över golvet
     if tilt1 != 0:
         B = (S - 1.6) / math.sin(math.radians(tilt1))
     else:
@@ -81,7 +80,7 @@ def getB(S, tilt1):
 def getPan2 (R, pan1, tilt1, B):
     Rtak = B*math.cos(math.radians(tilt1))
     Ctak = math.sqrt(((math.pow(R,2)) + ((math.pow(Rtak,2)) - 2 * R * Rtak * math.cos(math.radians(pan1)))))
-    pan2 = math.degrees(math.asin(Rtak*(math.sin(math.radians(pan1))/Ctak)))-pan20
+    pan2 = math.degrees(math.asin(Rtak*(math.sin(math.radians(pan1))/Ctak)))
     return str(pan2)
 
 # Beräkna tilt2
@@ -104,27 +103,7 @@ def move() :
 #Innan
 #K1: pan 127.51, tilt 0. K2: pan -126, tilt -6.2625
 
-def calibrate() :
-    r1 =requests.get(urlcam1+"query=position")
-    text1 = r1.text
-    textarray = text1.splitlines()
-    #pan10 = float(textarray[0].split('=')[1])
-    #tilt10 = float(textarray[1].split('=')[1])
-    pan10 = 127.51
-    pan20 = -126
-    tilt10 = 0
-    tilt20 = -6.2625
-    r2 =requests.get(urlcam2+"query=position")
-    text2 = r2.text
-    textarray2 = text2.splitlines()
-    #pan20 = float(textarray2[0].split('=')[1])
-    #tilt20 = float(textarray2[1].split('=')[1])
-    #print ("kamera 1: ","pan ",pan10," tilt ",tilt10,"kamera 2 ","pan ",pan20," tilt ",tilt20,)
 
-    r = requests.get(urlcam1+"tilt="+str(tilt10))
-    r = requests.get(urlcam1+"pan="+str(pan10))
-    r = requests.get(urlcam2+"tilt="+str(tilt20))
-    r = requests.get(urlcam2+"pan="+str(pan20))
 S = 1.8
 R = 1.2
 pan10 = -127.51
@@ -133,33 +112,44 @@ tilt10 = 0
 tilt20 = -6.2625
 
 calibrate()
+
 while 1!=0 :
 
     print("Bestäm pan")
-    pan1 = input()
+    pan1 = input() #input ges i riktiga koordinater
     #pan1 = "120"
 
     print ("Bestäm tilt") #input exit om du vill stänga
-    tilt1= input()
+    tilt1= input() #input ges i riktiga koordinater
 
     #tilt1 = "0"
-    if input() == "Calibrate":
-        calibrate()
-    #tilt1 = "30"
-<<<<<<< HEAD
-#    if input() == "exit":
-#        sys.exit()
-=======
-    #if(input() == "exit")
-    #    sys.exit()
->>>>>>> af52761acae960a2bc50bf1a900e5900f96cf243
+    #if input() == "Calibrate":
+    #    calibrate()
 
     print("du har valt "+pan1+" och "+ tilt1)
     pan1 = Decimal(pan1)
     tilt1 = Decimal(tilt1)
-    B = getB(S,tilt1)
-    pan2  = getPan2(R,pan1,tilt1,B)
-    tilt2 = getTilt2(R,pan1,tilt1,B,S)
-    print(pan2, tilt2)
-    move()
+    #B = getB(S,tilt1)
+    B=2
+
+    virPan1=0
+    if(pan10<0 && pan1<0){
+        virPan1=math.abs(pan10)-math.abs(pan1)
+    }
+    if(pan10>0 && pan1>0){
+        virPan1=math.abs(pan10)-math.abs(pan1)
+    }
+    if(pan10<0 && pan1>0){
+        virPan1=math.abs(pan10)+math.abs(pan1)
+    }
+    if(pan10>0 && pan1<0){
+        virPan1=-pan10+pan1
+    }
+
+    virPan2  = getPan2(R,virPan1,tilt1,B)
+
+    #tilt2 = getTilt2(R,pan1,tilt1,B,S)
+    #print(pan2, tilt2)
+    print(virPan2)
+    #move()
     continue
